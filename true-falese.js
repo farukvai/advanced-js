830f0bbaac8d06b0619dad0bf5b8e944e381d9ab
farukvai850@gmail.com <farukvai850@gmail.com>
2021-02-09 20:41:17 +0600

    true-false

---
+++ true-falese.js
@@ -0,0 +1,20 @@
+//false:
+//0
+//NaN
+// ""
+// undefined
+// False
+//null
+
+//true :
+// []
+// {}
+// ' '
+// true
+let name= null;
+
+if(name){
+    console.log('Condodtion is true');
+}else{
+    console.log('condition is false');
+}
