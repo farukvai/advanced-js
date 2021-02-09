acdc125746c5b4df4952ecb8ade44276bc1af213
farukvai850@gmail.com <farukvai850@gmail.com>
2021-02-09 21:30:08 +0600

    null vs undefinited

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+// let lover;
+
+// function add(x,y){
+//     console.log(x+y);
+//     return //ekaqne kiso na likhleu undifind hobe
+// }
+
+// function add(a,b){
+//     //return a+b;
+//     console.log(a,b);
+// }
+
+
+// const premik ={name:"faruk", gf:"Sima"};
+// console.log(premik.age);
+
+// let fun = undefined;
+// console.log(fun);
+
+// const age = [12,34,67];
+// console.log(age[7]);
