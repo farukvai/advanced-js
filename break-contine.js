ad3358ac3963734c9213879dbb08908275bca28f
farukvai850@gmail.com <farukvai850@gmail.com>
2021-02-22 15:19:23 +0600

    brak-continue

--- break-contine.js
+++ break-contine.js
@@ -9,13 +9,14 @@
     
 // }
 
-const nums = [1,-2,-3,2,3,4,-4,5,-5,6,7,8,9];
+// const nums = [1,-2,-3,2,3,4,-4,5,-5,6,7,8,9];
 
-for (let i = 0; i < nums.length; i++) {
-    const element = nums[i];
-    if(element<0){
-        continue;
-    }
-    console.log(element);
+// for (let i = 0; i < nums.length; i++) {
+//     const element = nums[i];
+//     if(element<0){
+//         continue;
+//     }
+//     console.log(element);
     
-}
+// }
+
